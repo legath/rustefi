89327e42fbced935bf2f9e9ef6039ff954df83a5
Alexander Bulychev <a.f.bulychev@gmail.com>
2022-12-03 05:14:51 +0300

    tms570 new commit

--- src/main.rs
+++ src/main.rs
@@ -9,17 +9,20 @@ extern crate tms570;
 extern crate alloc;
 extern crate linked_list_allocator;
 
-use tms570::serial::{SerialLine, Parity, StopBits, DataBits, Lines};
+use tms570::serial::{SerialLine, Parity, StopBits, DataBits, Lines, event};
 use tms570::scilin::SciChipset;
 use tms570::gio::{Gio, GioPorts, GioDirection, Pull};
 use tms570::iomm::Iomm;
 use tms570::pinmux::PinMux;
 use linked_list_allocator::LockedHeap;
 use alloc::vec::Vec;
+use core::arch::asm;
 use tms570::pwm::{Pwm, PwmId};
 use tms570::rti::RtiController;
+
 use tms570::syscounter::SysCounter;
-use tms570::system::Sys;
+use tms570::vim::{Vim, VimType};
+//use tms570::system::Sys;
 
 pub mod lang_items;
 pub mod handlers;
@@ -35,6 +38,10 @@ pub fn heap_init() {
     }
 }
 
+fn test_isr() {
+    unsafe { asm!("nop"); }
+}
+
 fn main() {
     unsafe {
         // Muxing pins is inherently unsafe
@@ -44,21 +51,29 @@ fn main() {
 
     heap_init();
 
+    /*unsafe {
+        let vim = Vim::new();
+        vim.isr_set(10, test_isr);
+        vim.set_type(10, VimType::SysInterrupt);
+        vim.interrupt_enable(10,true);
+    }*/
     let ioport = Gio::new();
-    let mut uart: SciChipset = SerialLine::new(Lines::Sci, DataBits::Eight,
+    let mut uart: SciChipset = SerialLine::new(Lines::Lin, DataBits::Eight,
                                                StopBits::One,
                                                Parity::None);
     uart.rx_enable(true)
         .tx_enable(true)
-        .set_baudrate(115_200);
+        .set_baudrate(9600);
+    //uart.interrupt(event::RX_INT);
     uart.open();
+    uart.write(b"Test\n");
 
     ioport.direction(GioPorts::MibSpiPort3, 0, GioDirection::Input);
     ioport.direction(GioPorts::B, 1, GioDirection::Output);
 
-    let syscnt: RtiController = SysCounter::new(false);
-    syscnt.set_period(0,10000);
-    syscnt.start_counter(0);
+    //let syscnt: RtiController = SysCounter::new(false);
+    //syscnt.set_period(0,10000);
+    //syscnt.start_counter(0);
 
     loop {
         let button = ioport.get(GioPorts::MibSpiPort3, 0);
